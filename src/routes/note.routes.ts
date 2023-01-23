8e271fb775b51ef5e287eb04ccd4a1a5d6ad0870
Vicente Albert <arizaz50@gmail.com>
2023-01-23 18:39:38 +0100

    Added endpoint to delete note

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { NoteShow, NoteStore, NoteUpdate } from '../http/controllers/Note/NoteController';
+import { NoteDelete, NoteShow, NoteStore, NoteUpdate } from '../http/controllers/Note/NoteController';
 
 const router = Router();
 
 router.get('/api/v1/note/:id', NoteShow);
 router.put('/api/v1/note/:id', NoteUpdate);
+router.delete('/api/v1/note/:id', NoteDelete);
 router.post('/api/v1/note', NoteStore);
 
 
