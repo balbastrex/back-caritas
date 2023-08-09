14e083aef5dd3b4a42e524019641b400fa743eb6
Vicente Albert <vicente.lopez.external@mytheresa.com>
2023-08-09 19:56:33 +0200

    implemented widgets for dashboard

--- src/http/controllers/Report/ReportController.ts
+++ src/http/controllers/Report/ReportController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express';
 import {Order} from "../../../entities/Order";
+import {Beneficiary} from "../../../entities/Beneficiary";
+import moment from 'moment';
+import {OrderLine} from "../../../entities/OrderLine";
+import {Product} from "../../../entities/Product";
+import {Brackets} from "typeorm";
+import {ParishOrdersReportResource} from "../Order/ParishOrdersReportResource";
+import {getBeneficiaryAmount, getParishAmount} from "../Receipt/ReceiptController";
 
 export const memoryPeriod = async (request: Request, response: Response) => {
   const startDate = request.body.startDate;
@@ -79,12 +86,15 @@ const getAmountOrders = async ({startDate, endDate, marketId}) => {
 
   let totalParish = 0;
   let totalBeneficiary = 0;
+  let total = 0;
   orders.forEach(order => {
+    total += order.amount;
     totalParish += ((order.gratuitous || 0) / 100) * order.amount;
     totalBeneficiary += order.amount - (((order.gratuitous || 0) / 100) * order.amount);
   })
 
   return {
+    total: total.toFixed(2),
     totalParish: totalParish.toFixed(2),
     totalBeneficiary: totalBeneficiary.toFixed(2),
   };
@@ -111,4 +121,90 @@ const getNumberOfFirstBeneficiaries = async ({startDate, endDate, marketId}) => 
     .getRawOne();
 
   return parseInt(numberOfFirstFamilies.firstFamilyOrders)
+}
+
+export const BeneficiariesByAge = async (request: Request, response: Response) => {
+  const marketId = response.locals.marketId;
+
+  const beneficiaries = await Beneficiary.createQueryBuilder('beneficiaries')
+    .leftJoin('beneficiaries.parish', 'parish')
+    .where('parish.marketId = :marketId', { marketId: marketId })
+    .select('SUM (beneficiaries.minors)', 'minors')
+    .addSelect('SUM (beneficiaries.adults)', 'adults')
+    .getRawOne();
+
+  return response.status(200).json(beneficiaries);
+}
+
+export const WeekReport = async (request: Request, response: Response) => {
+  const marketId = response.locals.marketId;
+  const startDate = moment().startOf('isoWeek').format('YYYY-MM-DD');
+  const endDate = moment().endOf('isoWeek').format('YYYY-MM-DD');
+
+  const most = await OrderLine.createQueryBuilder('orderLine')
+    .leftJoin('orderLine.order', 'order')
+    .where('order.id_economato = :economato', { economato: marketId })
+    .andWhere('order.created >= :start', { start: startDate })
+    .andWhere('order.created <= :end', { end: endDate })
+    .select('orderLine.productId', 'productId')
+    .addSelect('SUM (orderLine.units)', 'units')
+    .groupBy('orderLine.productId')
+    .orderBy('units', 'DESC')
+    .getRawOne();
+
+  const productName = await Product.findOne(most.productId)
+
+  const mostSold = {
+    product: productName.name,
+    units: most.units,
+  }
+
+  const amountBeneficiariesUF = await getNumberOfBeneficiaries({startDate, endDate, marketId});
+  const amountOrders = await getAmountOrders({startDate, endDate, marketId});
+  const totalBeneficiaries = await getNumberOfLicense({startDate, endDate, marketId});
+
+  return response.status(200).json({
+    numberOfOrders: totalBeneficiaries,
+    totalAmountOrders: amountOrders.total,
+    mostSold,
+    amountBeneficiariesUF: amountBeneficiariesUF.numberOfFamilyUnit
+  });
+}
+
+export const ParishWeekReport = async (request: Request, response: Response) => {
+  const marketId = response.locals.marketId;
+  const startDate = moment().startOf('isoWeek').format('YYYY-MM-DD');
+  const endDate = moment().endOf('isoWeek').format('YYYY-MM-DD');
+
+  const query = Order.createQueryBuilder('orders')
+    .leftJoinAndSelect('orders.beneficiary', 'beneficiary')
+    .leftJoinAndSelect('beneficiary.parish', 'parish')
+    .leftJoinAndSelect('orders.market', 'market')
+    .where('orders.id_economato = :economato', { economato: marketId })
+
+    query.andWhere('orders.created >= :start', { start: startDate })
+    query.andWhere('orders.created <= :end', { end: endDate });
+
+  const orders: Array<any> = await query.orderBy({
+    'orders.id': 'ASC'
+  })
+    .getMany();
+
+  const mySet = new Set();
+  const distinct = orders.filter((order) => {
+    const duplicate = mySet.has(order.beneficiary.parish.id);
+    mySet.add(order.beneficiary.parish.id);
+    return !duplicate;
+  })
+
+  const result = distinct.map((dis) => {
+    const ordersByParish = orders.filter((order) => order.beneficiary.parish.id === dis.beneficiary.parish.id);
+
+    return {
+      parishName: dis.beneficiary.parish.name,
+      numOrders: ordersByParish.length,
+    }
+  });
+
+  return response.status(200).json(result);
 }
--- src/routes/report.routes.ts
+++ src/routes/report.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
-import { memoryPeriod } from '../http/controllers/Report/ReportController';
+import {
+  BeneficiariesByAge,
+  memoryPeriod,
+  ParishWeekReport,
+  WeekReport
+} from '../http/controllers/Report/ReportController';
 
 const router = Router();
 
 router.post('/api/v1/report/memory-period', memoryPeriod);
+router.get('/api/v1/report/beneficiaries-age-report', BeneficiariesByAge);
+router.get('/api/v1/report/week', WeekReport);
+router.get('/api/v1/report/parish-week', ParishWeekReport);
 
 export default router;
